181345a2be01954a9f9766b914568ba735adba28
YHWHisKing <djtembo4@gmail.com>
2026-04-08 12:07:44 +0200

    dependencies added

---
+++ .vite/deps/react-icons.js
@@ -0,0 +1,16 @@
+import {
+  DefaultContext,
+  GenIcon,
+  IconBase,
+  IconContext,
+  IconsManifest
+} from "./chunk-SNMORBIY.js";
+import "./chunk-P6RTVJOB.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  DefaultContext,
+  GenIcon,
+  IconBase,
+  IconContext,
+  IconsManifest
+};
